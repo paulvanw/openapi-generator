8b5a7edeb1619b81ac511c9818d16468088ff363
Paul <paul@wingmen.io>
2019-10-09 00:29:06 +0200

    wip! repro supress CA for generated code
    https://docs.microsoft.com/en-us/visualstudio/code-quality/how-to-suppress-code-analysis-warnings-for-generated-code?view=vs-2019

---
+++ repro/sample/Program.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Sample
+{
+    using System;
+
+    /// <summary>
+    /// test application to see if we can supress CA in generated files.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The entrypoint of our console application.
+        /// </summary>
+        /// <param name="args">arguments to be passed to the application.</param>
+        private static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello World! {args.Length}");
+        }
+    }
+}
---
+++ repro/sample/generated.cs
@@ -0,0 +1,23 @@
+﻿// Generated
+// <copyright file="generated.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Sample
+{
+    /// <summary>
+    /// Removing the GeneratedCode attribute breaks the build
+    /// </summary>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("openapi-generator", "1.0")]
+    public class Generated
+    {
+        /// <summary>
+        /// This method breeks (SA1503 Braces should not be omitted).
+        /// </summary>
+        public void BreekieBreek()
+        {
+            if (true)
+                System.Console.WriteLine("We love braces");
+        }
+    }
+}
